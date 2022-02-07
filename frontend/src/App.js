3e3e4bd19fd794d4bb85a76813642931fd8fe438
Kevin Rufino <57162630+kevinrufino@users.noreply.github.com>
2022-02-07 09:10:58 -0500

    removed unused var

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { lazy, Suspense } from 'react';
-import Particles from "react-tsparticles";
 
 const Header = lazy(() => import('./components/Header'));
 const Hero = lazy(() => import('./components/Hero'));
